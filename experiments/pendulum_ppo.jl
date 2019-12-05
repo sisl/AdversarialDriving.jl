8ff2dcfae39db7a65447dd3740a7cad9ca589634
Anthony Corso <anthonycorso92@gmail.com>
2019-12-05 11:01:00 -0800

    tested version of pendulum v0

--- experiments/pendulum_ppo.jl
+++ experiments/pendulum_ppo.jl
@@ -2,7 +2,7 @@ include("../simulator/gym_continous.jl")
 include("../solver/ppo.jl")
 using Flux: ADAM
 using Flux.Tracker: gradient, update!, hook
-using LinearAlgebra
+using LinearAlgebra, Plots
 
 env = make("Pendulum-v0", :human_pane)
 env._env.action_space
@@ -17,37 +17,24 @@ params = to_params(policy)
 opt = ADAM(0.001, (0.9, 0.999))
 loss() = ppo_batch_loss(env, policy, 125, 1., 0.95, 1.)
 
-obs_sum = zeros(input_size)
-obs_sumsq = zeros(input_size)
-obs_N = 0
-ret_arr = [start_ret]
-
-N = 100
+N, max_norm = 100, 1.
 for i=1:N
     grads = gradient(() -> loss(), params)
-    update_with_clip!(opt, grads, params, 1.)
+    update_with_clip!(opt, grads, params, max_norm)
 
-    println("Finished epoch, ", i, " return: ", episode_returns(env, policy, 10), " grad norms: ", gnorm)
+    println("Finished epoch, ", i, " return: ", episode_returns(env, policy, 10), " grad norms: ", clipped_grad_norms(grads, params, max_norm))
 end
 
-i = 1
-done = false
-o = reset!(env)
-tot_r = 0
+i, done, o, tot_r = 1, false, reset!(env), 0
 theta, dtheta = [], []
 while !done
-    global i
     a = sample_action(policy, o)
-    println("i=", i, " action: ", a)
     global o, r, done, d = step!(env, a)
     global tot_r += r
     push!(theta, env._env.state[1])
     push!(dtheta, env._env.state[2])
-    # render!(env)
-    i += 1
-    if i > 201
-        done = true
-    end
+    global i += 1
+    (i > env.max_episode_steps) && break
 end
 println("total reward: ", tot_r)
 
--- solver/optimization.jl
+++ solver/optimization.jl
@@ -16,11 +16,11 @@ end
 
 # Returns the clipped value of the gradient norms
 # used for diagnosing
-clipped_grad_norms(grads, params, max_norm, p=2) = min(max_norm, grad_norms(grads, params, p=p))
+clipped_grad_norms(grads, params, max_norm, p=2) = min(max_norm, grad_norms(grads, params, p))
 
 # update the parameters with gradient clipping
 function update_with_clip!(opt, grads, params, max_norm, p=2)
-    scale = clip_norm_scale(grads, params, max_norm, p=p)
+    scale = clip_norm_scale(grads, params, max_norm, p)
     for p in params
         update!(opt, p, scale*grads[p])
     end
